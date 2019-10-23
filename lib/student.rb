316a7a32704a55669a676d3773c3576b079b935d
Wendy Hamilton <wdhamilton117@gmail.com>
2019-10-23 14:37:18 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -13,6 +13,14 @@ class Student
   end
 
   def self.create_table
+    sql =  <<-SQL 
+      CREATE TABLE IF NOT EXISTS songs (
+        id INTEGER PRIMARY KEY, 
+        name TEXT, 
+        album TEXT
+        )
+        SQL
+    DB[:conn].execute(sql) 
   end
 
   def self.drop_table

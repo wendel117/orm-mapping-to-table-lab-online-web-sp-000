ae6757f636630ef910b732ca12cc06698b4e55d5
Wendy Hamilton <wdhamilton117@gmail.com>
2019-10-23 14:38:49 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,10 +10,10 @@ class Student
 
   def save
     sql = <<-SQL
-          INSERT INTO songs (name, album) 
+          INSERT INTO songs (name, album)
           VALUES (?, ?)
         SQL
-     
+
         DB[:conn].execute(sql, self.name, self.grade)
   end
 
